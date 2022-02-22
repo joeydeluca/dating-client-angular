a2b53bbddf269e63e54178bcb9d65e73fb5482c9
joeydeluca <joeydeluca@hotmail.com>
2022-02-22 14:37:21 -0500

    fix upgrade bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,35 @@
-import { Component, HostBinding } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
 import {NgbDatepickerConfig} from '@ng-bootstrap/ng-bootstrap';
 import {SharedService} from './services/shared.service';
+import {UserService} from './services/user.service';
+import {AuthService} from './services/auth.service';
 import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   showLoader = false;
 
   public get childRouteTransition() { return this.activatedRoute.snapshot; }
 
-  constructor(public activatedRoute: ActivatedRoute ) {
+  constructor(public activatedRoute: ActivatedRoute, private userService: UserService, private authService: AuthService) {
     SharedService.showLoader
     .subscribe(val => this.showLoader = val);
   }
+
+  ngOnInit() {
+    if (!this.authService.getAuthContext()) return;
+
+    this.userService.isPaid().subscribe((isPaid: boolean) => {
+      if (isPaid) {
+        this.authService.refreshContext().subscribe();
+        this.userService.getUserFromServer().subscribe();
+        SharedService.showUpgradeButton.next(false);
+      }
+    })
+  }
+
 }
